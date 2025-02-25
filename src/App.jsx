96e5729b3ff197453434f6a008e2e8fc6c7f0364
Ahsan Ahmed <ahsanahmed9104@gmail.com>
2025-02-26 00:55:22 +0530

    scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import AppRoutes from './routes/approutes'
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/header'
+import ScrollToTop from './components/scrolltotop'
 
 
 const App = () => {
   return (
     <>
     
+    <ScrollToTop/>
 
      <AppRoutes/>
      <Header/>
